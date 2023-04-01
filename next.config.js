2d7fc11e3cf2b15b372ffc0a2f2fc0e23cbf6d9c
ayodejipy <ay.programmr@gmail.com>
2023-04-01 11:46:50 +0100

    update: added image domain to config file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['tailwindui.com', 'cdn.sanity.io']
+    domains: ['tailwindui.com', 'cdn.sanity.io', 'lh3.googleusercontent.com']
   }
 }
 
